442202291909314d278fea679124ffa7c2afc21c
Jose Montes Petour <josemontesp@gmail.com>
2015-10-28 16:37:20 -0300

    select desaparece con boton

--- www/scripts/services.js
+++ www/scripts/services.js
@@ -187,4 +187,25 @@ angular.module('starter.services', [])
             }
 
         }
-    ]);
+    ])
+.directive('select', function($timeout) {
+  return {
+    restrict: 'E',
+    link: function(_, element) {
+      element.bind('focus', function(e) {
+        $timeout(function() {
+          if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+          }
+        })
+      });
+      element.bind('blur', function(e) {
+        $timeout(function() {
+          if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          }
+        });
+      });
+    }
+  }
+});
