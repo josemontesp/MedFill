87bc90362611a2b162be240a346f61a524a73d4a
Jose Montes Petour <josemontesp@gmail.com>
2015-11-03 23:13:21 -0300

    text area autoexpandible

--- www/scripts/services.js
+++ www/scripts/services.js
@@ -208,4 +208,18 @@ angular.module('starter.services', [])
       });
     }
   }
+}).directive('textarea', function() {
+  return {
+    restrict: 'E',
+    link: function(scope, element, attr){
+        var update = function(){
+            element.css("height", "auto");
+            var height = element[0].scrollHeight; 
+            element.css("height", element[0].scrollHeight + "px");
+        };
+        scope.$watch(attr.ngModel, function(){
+            update();
+        });
+    }
+  };
 });
